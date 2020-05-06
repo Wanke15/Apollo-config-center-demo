94420d7104511a8abd02c69d755dbec09b04ab86
Jeff <1015005300@qq.com>
2020-05-06 23:55:15 +0800

    ugly workaround using dead while loop

--- logic.py
+++ logic.py
@@ -1,9 +1,21 @@
+import time
+
+import threading
+
 from pyapollo import ApolloClient
 
 client = ApolloClient('Mengniu-Rec-test')
 
 
+def client_loop(loop_interval=3):
+    while True:
+        client.start()
+        time.sleep(loop_interval)
+
+
+threading.Thread(target=client_loop, args=(3, )).start()
+
+
 def recommend():
-    client.start()
     v = client.get_value("source_id_weight")
     return v
